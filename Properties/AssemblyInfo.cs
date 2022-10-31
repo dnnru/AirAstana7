8987eb735ff8046c735225e6167197d8a6fa1d11
George Saveliev <george_saveliev@hotmail.com>
2022-10-31 04:46:59 +0300

    fix names

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AirAstana7")]
-[assembly: AssemblyCopyright("Air Astana Copyright © 2021")]
+[assembly: AssemblyCopyright("Air Astana Copyright © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("01.00.20.00")]
-[assembly: AssemblyFileVersion("01.00.20.00")]
+[assembly: AssemblyVersion("01.00.22.00")]
+[assembly: AssemblyFileVersion("01.00.22.00")]
